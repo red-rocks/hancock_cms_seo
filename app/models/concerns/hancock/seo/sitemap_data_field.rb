671794b895531c9d6f69e9104fb8f025cf86c484
Alexander Kiseliev <ack@CaH4aC>
2016-09-14 13:24:58 +0300

    sitemap gem order; default sitemap

--- app/models/concerns/hancock/seo/sitemap_data_field.rb
+++ app/models/concerns/hancock/seo/sitemap_data_field.rb
@@ -13,5 +13,25 @@ module Hancock::Seo::SitemapDataField
     def sitemap_data
       sitemap_data_without_build || build_sitemap_data
     end
+
+    def default_sitemap_changefreq
+      'weekly'
+    end
+    def default_sitemap_priority
+      0.8
+    end
+
+    def to_sitemap(sitemap)
+      _sitemap_data = self.sitemap_data
+      _lastmod = _sitemap_data.sitemap_lastmod.to_date  unless _sitemap_data.sitemap_lastmod.nil?
+      _lastmod = self.updated_at.to_date                unless self.updated_at.nil?        if _lastmod.nil?
+      _lastmod = self.created_at.to_date                unless self.created_at.nil?        if _lastmod.nil?
+
+      sitemap.add sitemap.url_for(i),
+          :lastmod      => _lastmod,
+          :changefreq   => "#{_sitemap_data.sitemap_changefreq.blank? ? self.default_changefreq       : _sitemap_data.sitemap_changefreq}",
+          :priority     => (_sitemap_data.sitemap_priority.nil?       ? self.default_sitemap_priority : _sitemap_data.sitemap_priority)
+    end
+
   end
 end
--- hancock_cms_seo.gemspec
+++ hancock_cms_seo.gemspec
@@ -30,9 +30,9 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.10"
   spec.add_development_dependency "rake", "~> 10.0"
 
-  spec.add_dependency 'hancock_cms', [">=0.1.0", "<=1.0.0"]
-  # spec.add_dependency 'hancock_cms', ["~> 0.1.0", "~> 1.0.0"]
-
   spec.add_dependency "sitemap_generator"
   spec.add_dependency "rails_admin_sitemap"
+
+  spec.add_dependency 'hancock_cms', [">=0.1.0", "<=1.0.0"]
+  # spec.add_dependency 'hancock_cms', ["~> 0.1.0", "~> 1.0.0"]
 end
