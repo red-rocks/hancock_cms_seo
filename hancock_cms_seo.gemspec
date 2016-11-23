87d8c5138f8d9249edf7b1a6ce9ac29397666cb2
Alexander Kiseliev <i43ack@gmail.com>
2016-11-23 10:38:44 +0300

    generators upd; dependencies

--- hancock_cms_seo.gemspec
+++ hancock_cms_seo.gemspec
@@ -30,9 +30,9 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.10"
   spec.add_development_dependency "rake", "~> 10.0"
 
+  spec.add_dependency 'hancock_cms', [">=1.0", "<=2.1"]
+  # spec.add_dependency 'hancock_cms', ["~> 1.0", "~> 2.1"]
+  
   spec.add_dependency "sitemap_generator", '~> 5.2.0'
   spec.add_dependency "rails_admin_sitemap", '~> 0.2.0'
-
-  spec.add_dependency 'hancock_cms', [">=1.0", "<=2.0"]
-  # spec.add_dependency 'hancock_cms', ["~> 1.0", "~> 2.0"]
 end
--- lib/generators/hancock/seo/migrations/migrations_generator.rb
+++ lib/generators/hancock/seo/migrations/migrations_generator.rb
@@ -2,12 +2,12 @@ require 'rails/generators'
 require 'rails/generators/active_record'
 
 module Hancock::Seo
-  class MigrationGenerator < Rails::Generators::Base
+  class MigrationsGenerator < Rails::Generators::Base
     include ActiveRecord::Generators::Migration
     source_root File.expand_path('../templates', __FILE__)
 
     desc 'Hancock SEO migration generator'
-    def migration
+    def migrations
       if Hancock.active_record?
         %w(seos).each do |table_name|
           migration_template "migration_#{table_name}.rb", "db/migrate/hancock_create_#{table_name}.rb"
