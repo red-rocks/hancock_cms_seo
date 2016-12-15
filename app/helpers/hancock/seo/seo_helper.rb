3366f59ba196ab8ade817676f35faeb13a279bb0
Alexander Kiseliev <i43ack@gmail.com>
2016-12-15 14:07:41 +0300

    additional cachekeys for counters

--- app/helpers/hancock/seo/seo_helper.rb
+++ app/helpers/hancock/seo/seo_helper.rb
@@ -11,6 +11,8 @@ module Hancock::Seo::SeoHelper
 
     _cache_key = "ym_counter".freeze
     ym_counter = Rails.cache.fetch(_cache_key) do
+      _add_cache_key = opts.delete(:cache_key)
+      _cache_key = [_cache_key, _add_cache_key].flatten if _add_cache_key
       ym_counter_id = opts[:ym_counter_id] || Settings.ym_counter_id(default: '', kind: :string, label: 'Yandex Метрика ID'.freeze, cache_keys: _cache_key).strip
       if ym_counter_id.blank?
         Settings.ym_counter_html(default: '', kind: :code, label: 'Yandex Метрика HTML-код'.freeze, cache_keys: _cache_key).strip
@@ -22,6 +24,8 @@ module Hancock::Seo::SeoHelper
 
     _cache_key = "ga_counter".freeze
     ga_counter = Rails.cache.fetch(_cache_key) do
+      _add_cache_key = opts.delete(:cache_key)
+      _cache_key = [_cache_key, _add_cache_key].flatten if _add_cache_key
       ga_counter_id = opts[:ga_counter_id] || Settings.ga_counter_id(default: '', kind: :string, label: 'Google Analitics ID'.freeze, cache_keys: _cache_key).strip
       if ga_counter_id.blank?
         Settings.ga_counter_html(default: '', kind: :code, label: 'Google Analitics HTML-код'.freeze, cache_keys: _cache_key).strip
