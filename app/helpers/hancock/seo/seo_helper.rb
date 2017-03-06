2cdc787e17b407f4ce474140e740be0411c43c16
Alexander Kiseliev <i43ack@gmail.com>
2017-03-06 10:48:11 +0300

    counters on cache

--- app/helpers/hancock/seo/seo_helper.rb
+++ app/helpers/hancock/seo/seo_helper.rb
@@ -11,12 +11,21 @@ module Hancock::Seo::SeoHelper
 
     _cache_key = "ym_counter".freeze
     ym_counter = Rails.cache.fetch(_cache_key) do
+      if Hancock::Seo.config.cache_support
+        Hancock::Cache::Fragment.create_unless_exists(name: _cache_key, parents: hancock_cache_views_keys)
+        _cache_key = [_cache_key]
+        _cache_key += hancock_cache_views_keys
+      else
+        _cache_key = [_cache_key]
+      end
       _add_cache_key = opts.delete(:cache_key)
       _cache_key = [_cache_key, _add_cache_key].flatten if _add_cache_key
-      ym_counter_id = opts[:ym_counter_id] || Settings.ym_counter_id(default: '', kind: :string, label: 'Yandex Метрика ID'.freeze, cache_keys: _cache_key).strip
-      if ym_counter_id.blank?
-        Settings.ym_counter_html(default: '', kind: :code, label: 'Yandex Метрика HTML-код'.freeze, cache_keys: _cache_key).strip
+
+      _html = Settings.ym_counter_html(default: '', kind: :code, label: 'Yandex Метрика HTML-код'.freeze, cache_keys: _cache_key).strip
+      unless _html.blank?
+        _html
       else
+        ym_counter_id = opts[:ym_counter_id] || Settings.ym_counter_id(default: '', kind: :string, label: 'Yandex Метрика ID'.freeze, cache_keys: _cache_key).strip
         hancock_ym_counter_tag(ym_counter_id)
       end
     end
@@ -24,12 +33,21 @@ module Hancock::Seo::SeoHelper
 
     _cache_key = "ga_counter".freeze
     ga_counter = Rails.cache.fetch(_cache_key) do
+      if Hancock::Seo.config.cache_support
+        Hancock::Cache::Fragment.create_unless_exists(name: _cache_key, parents: hancock_cache_views_keys)
+        _cache_key = [_cache_key]
+        _cache_key += hancock_cache_views_keys
+      else
+        _cache_key = [_cache_key]
+      end
       _add_cache_key = opts.delete(:cache_key)
       _cache_key = [_cache_key, _add_cache_key].flatten if _add_cache_key
-      ga_counter_id = opts[:ga_counter_id] || Settings.ga_counter_id(default: '', kind: :string, label: 'Google Analitics ID'.freeze, cache_keys: _cache_key).strip
-      if ga_counter_id.blank?
-        Settings.ga_counter_html(default: '', kind: :code, label: 'Google Analitics HTML-код'.freeze, cache_keys: _cache_key).strip
+
+      _html = Settings.ga_counter_html(default: '', kind: :code, label: 'Google Analitics HTML-код'.freeze, cache_keys: _cache_key).strip
+      unless _html.blank?
+        _html
       else
+        ga_counter_id = opts[:ga_counter_id] || Settings.ga_counter_id(default: '', kind: :string, label: 'Google Analitics ID'.freeze, cache_keys: _cache_key).strip
         hancock_ga_counter_tag(ga_counter_id)
       end
     end
