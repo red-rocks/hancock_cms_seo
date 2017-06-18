fc5203bed108787be7c31b5668d055d44dc1e27e
Alexander Kiseliev <i43ack@gmail.com>
2017-06-18 18:04:08 +0300

    events helper refactoring and fixes

--- app/helpers/hancock/seo/events_helper.rb
+++ app/helpers/hancock/seo/events_helper.rb
@@ -1,13 +1,15 @@
 module Hancock::Seo::EventsHelper
 
   def render_seo_events_functions
-    Hancock::Seo::Event.enabled.sorted.to_a.map { |e|
+    Hancock::Seo::Event.enabled.sorted.to_a.map do |e|
       code  = []
+      function_name = "window.#{e.function_name}"
+      code << "#{function_name} || (#{function_name}= function(e){#{e.listener_code}});"
       code << "window.#{e.function_name} || (window.#{e.function_name} = function(e){#{e.listener_code}});"
       # code << "function #{e.function_name}(e){#{e.listener_code_with_insertions}};"
       e.event_types.each do |type|
         next if type.blank? or type.strip.blank?
-        add_event_listener_code = "addEventListener('#{type}', window.#{e.function_name});alert('addEventListener #{e.function_name}');"
+        add_event_listener_code = "addEventListener('#{type}', window.#{e.function_name});"
         if e.target_selector.blank?
           event_elem = "document"
           code << "#{event_elem}.#{add_event_listener_code}"
@@ -24,36 +26,37 @@ module Hancock::Seo::EventsHelper
         end
       end
       code
-    }.join.html_safe
+    end.join.html_safe
   end
 
   def render_seo_events_tags
     code  = []
     reset_events_function_name = Hancock::Seo.config.reset_events_function_name
     code << "#{reset_events_function_name} || (#{reset_events_function_name} = function(){"
-    Hancock::Seo::Event.enabled.sorted.to_a.each { |e|
-      function_name = "window.#{e.function_name}"
-      code << "#{function_name} || (#{function_name}= function(e){#{e.listener_code}});"
-      # code << "function #{e.function_name}(e){#{e.listener_code_with_insertions}};"
-      e.event_types.each do |type|
-        next if type.blank? or type.strip.blank?
-        add_event_listener_code = "addEventListener('#{type}', #{e.function_name});"
-        if e.target_selector.blank?
-          event_elem = "document"
-          code << "#{event_elem}.#{add_event_listener_code}"
-        else
-          event_elem = "document.#{e.selector_function}('#{e.target_selector}')"
-          code << case e.selector_function
-          when 'querySelectorAll'
-            "#{event_elem}.forEach(function(){this.#{add_event_listener_code}});"
-          when 'querySelector'
-            "#{event_elem}.#{add_event_listener_code}"
-          else
-            "#{event_elem}.#{add_event_listener_code}"
-          end
-        end
-      end
-    }
+    code << render_seo_events_functions
+    # Hancock::Seo::Event.enabled.sorted.to_a.each do |e|
+    #   function_name = "window.#{e.function_name}"
+    #   code << "#{function_name} || (#{function_name}= function(e){#{e.listener_code}});"
+    #   # code << "function #{e.function_name}(e){#{e.listener_code_with_insertions}};"
+    #   e.event_types.each do |type|
+    #     next if type.blank? or type.strip.blank?
+    #     add_event_listener_code = "addEventListener('#{type}', #{e.function_name});"
+    #     if e.target_selector.blank?
+    #       event_elem = "document"
+    #       code << "#{event_elem}.#{add_event_listener_code}"
+    #     else
+    #       event_elem = "document.#{e.selector_function}('#{e.target_selector}')"
+    #       code << case e.selector_function
+    #       when 'querySelectorAll'
+    #         "#{event_elem}.forEach(function(){this.#{add_event_listener_code}});"
+    #       when 'querySelector'
+    #         "#{event_elem}.#{add_event_listener_code}"
+    #       else
+    #         "#{event_elem}.#{add_event_listener_code}"
+    #       end
+    #     end
+    #   end
+    # end
     code << "}); #{reset_events_function_name}();"
     javascript_tag code.join.html_safe, defer: true
   end
