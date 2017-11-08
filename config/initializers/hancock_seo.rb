804a1fc20aac5266266cce699998648d7ac2841f
Alexander Kiseliev <i43ack@gmail.com>
2017-11-08 17:43:59 +0300

    action_visible_for fix

--- config/initializers/hancock_seo.rb
+++ config/initializers/hancock_seo.rb
@@ -1,7 +1,7 @@
 Hancock.rails_admin_configure do |config|
-  config.action_visible_for :sitemap, Proc.new { false }
+  config.action_visible_for :sitemap, Proc.new { true }
   config.action_visible_for :sitemap_for_model, 'Hancock::Seo::SitemapData'
 
-  config.action_visible_for :robots_txt, Proc.new { false }
+  config.action_visible_for :robots_txt, Proc.new { true }
   config.action_visible_for :robots_txt_for_model, 'Hancock::Seo::Seo'
 end
